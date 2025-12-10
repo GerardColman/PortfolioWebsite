0bd5a990b6cfb6a83f45e40cf806227d2f27e2ef
Gerard Colman <gerard.colman1@gmail.com>
2025-12-11 01:02:00 +0900

    init

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+
+import './App.css'
+
+function App() {
+
+  return (
+    // <BrowserRouter>
+
+    //   {/* {/* Navigation
+    //   <nav id="navbar">
+    //     <Link to="/">Home</Link> |{" "}
+    //     <Link to="/music">Portfolio</Link> |{" "}
+    //     <Link to="/programming">Github</Link> |{" "}
+    //     <Link to="/contact">Contact</Link>
+    //   </nav> */}
+
+    //   {/* Routes */}
+    //   <Routes>
+    //     <Route path="/" element={<Home />} />
+    //     <Route path="/music" element={<MusicPortfolio />} />
+    //     <Route path="/programming" element={<ProgrammingPortfolio />} />
+    //     <Route path="/contact" element={<Contact />} />
+    //   </Routes> */
+
+    // </BrowserRouter>
+    <p>Hello world</p>
+  )
+}
+
+export default App
